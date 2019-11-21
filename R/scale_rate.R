714dd95461d186f6c7ee37871fd0a7ca7d9c14f4
nicholascarey <nicholascarey@gmail.com>
2019-11-21 12:57:54 +0000

    minor stuff

---
+++ R/scale_rate.R
@@ -0,0 +1,72 @@
+#' @title Scale rate
+#'
+#' @description \code{scale_rate} - Scales a physiological rate (absolute or
+#'   mass-specific) to a different mass, using a specified scaling exponent.
+#'
+#' @details Physiological rates typically scale allometrically with body mass.
+#'   Therefore, it is usually not appropriate to directly compare rates between
+#'   individuals of different size. Instead, if the scaling exponent of the rate
+#'   (either absolute or mass-specific) is known, this can be used to scale
+#'   rates to a common mass for comparison or statistical testing.
+#'
+#'   `scale_rate` scales an absolute (i.e. whole animal) or mass-specific
+#'   physiological rate (e.g. metabolic, feeding) to a specified body mass,
+#'   using a specified scaling exponent. **Important note**: the scaling
+#'   exponent should be the correct one for the rate used. For example, in the
+#'   case of metabolism, absolute (whole animal) rates typically scale at values
+#'   between +0.67 to +1 (i.e. between two-thirds and one), and mass-specific
+#'   rates between -0.33 to 0. Therefore, make sure you use the correct scaling
+#'   exponent for the `rate` as entered, INCLUDING THE -/+ SIGN.
+#'
+#'   The scaled rate is calculated as `((new mass/actual mass) ^ b) * rate`.
+#'
+#' @usage scale_rate(rate, mass, new.mass, b)
+#'
+#' @param rate numeric. The physiological rate to be scaled.
+#' @param mass numeric. Original mass at which the `rate` was determined.
+#' @param new.mass numeric. The new mass the `rate` is being scaled to.
+#' @param b numeric. Scaling exponent. Must be the correct one for the `rate`
+#'   used. See Details.
+#'
+#' @return numeric. New rate scaled to chosen mass
+#'
+#' @examples
+#' ## Scaling a whole animal metabolic rate.
+#' ## Here the rate is in mg/h, the metabolic scaling exponent is 0.72, and
+#' ## we are scaling the rate to 0.5g.
+#' scale_rate(0.1784, mass = 0.0847, new.mass = 0.5, b = 0.72)
+#'
+#' ## Result = 0.6406 mg/h at 0.5g
+#'
+#' ## Here the exact same calculation is done using the mass-specific
+#' ## metabolic rate (0.1784/0.0847 = 2.1063 mg/h/g), and the mass-specific
+#' ## metabolic scaling exponent (the negative difference from 1 of the
+#' ## absolute scaling exponent).
+#' scale_rate(2.1063, mass = 0.0847, new.mass = 0.5, b = -0.28)
+#'
+#' ## Result = 1.2812 mg/h/g at 0.5g
+#'
+#' ## We can see this is the same result as in the first example if it is
+#' ## expressed as a mass-specific rate:
+#' ## 0.6406/0.5 = 1.2812 mg/h/g
+#'
+#' ## Obviously, scaling a rate to the same mass will give the same rate.
+#' scale_rate(32.55, mass = 1, new.mass = 1, b = 0.75)
+#'
+#' ## And these demonstrate that isometric (i.e. purely linear) scaling of
+#' ## a rate will do a simple multiplication/division.
+#' scale_rate(5, mass = 0.5, new.mass = 1, b = 1)
+#' scale_rate(20, mass = 2, new.mass = 1, b = 1)
+#'
+#' @author Nicholas Carey - \email{nicholascarey@gmail.com}
+#'
+#' @export
+
+scale_rate <- function(rate = NULL, mass = NULL, new.mass = NULL, b = NULL) {
+
+  if(length(c(rate, mass, new.mass, b)) != 4) stop("All four inputs are required.")
+
+  new.rate <- ((new.mass / mass) ^ b) * rate
+
+  return(new.rate)
+}
